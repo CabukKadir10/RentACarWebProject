460d8f400fa6f6774614207c750f6a68f71fae27
kadircbk <kadir.c@stratejibilisim.com.tr>
2024-06-01 13:50:54 +0300

    mappleme işlemi yapıldı. endpointler test edildi. açlışıyor hepsi

--- src/RentACarProject.Application/RentACarProjectApplicationAutoMapperProfile.cs
+++ src/RentACarProject.Application/RentACarProjectApplicationAutoMapperProfile.cs
@@ -1,4 +1,16 @@
 ﻿using AutoMapper;
+using RentACarProject.Brands.Dtos;
+using RentACarProject.Cars.Dtos;
+using RentACarProject.Colors.Dtos;
+using RentACarProject.Entities.Brands;
+using RentACarProject.Entities.Cars;
+using RentACarProject.Entities.Colors;
+using RentACarProject.Entities.Fuels;
+using RentACarProject.Entities.Models;
+using RentACarProject.Entities.Transmissions;
+using RentACarProject.Fuels.Dtos;
+using RentACarProject.Models.Dtos;
+using RentACarProject.Transmissions.Dtos;
 
 namespace RentACarProject;
 
@@ -9,5 +21,30 @@ public class RentACarProjectApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        CreateMap<Brand, UpdatedBrandDto>().ReverseMap();
+        CreateMap<Brand, BrandDto>().ReverseMap();
+        CreateMap<Brand, CreatedBrandDto>().ReverseMap();
+
+        CreateMap<Car, CarDto>().ReverseMap();
+        CreateMap<Car, CreatedCarDto>().ReverseMap();
+        CreateMap<Car, UpdatedCarDto>().ReverseMap();
+
+        CreateMap<Color, ColorDto>().ReverseMap();
+        CreateMap<Color, CreatedColorDto>().ReverseMap();
+        CreateMap<Color, UpdatedColorDto>().ReverseMap();
+
+        CreateMap<Fuel, FuelDto>().ReverseMap();
+        CreateMap<Fuel, CreatedFuelDto>().ReverseMap();
+        CreateMap<Fuel, UpdatedFuelDto>().ReverseMap();
+
+        CreateMap<Model, ModelDto>().ReverseMap();
+        CreateMap<Model, CreatedModelDto>().ReverseMap();
+        CreateMap<Model, UpdatedModelDto>().ReverseMap();
+
+        CreateMap<Transmission, TransmissionDto>().ReverseMap();
+        CreateMap<Transmission, CreatedTransmissionDto>().ReverseMap();
+        CreateMap<Transmission, UpdatedTransmissionDto>().ReverseMap();
+
     }
 }
