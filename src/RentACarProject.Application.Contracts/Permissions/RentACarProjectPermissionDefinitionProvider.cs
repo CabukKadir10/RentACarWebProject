703d77b2f462ff21ff2f2f4b59ed2f071297a9b3
kadircbk <kadir.c@stratejibilisim.com.tr>
2024-06-02 14:51:23 +0300

    izinler ayarlandı

--- src/RentACarProject.Application.Contracts/Permissions/RentACarProjectPermissionDefinitionProvider.cs
+++ src/RentACarProject.Application.Contracts/Permissions/RentACarProjectPermissionDefinitionProvider.cs
@@ -11,6 +11,77 @@ public class RentACarProjectPermissionDefinitionProvider : PermissionDefinitionP
         var myGroup = context.AddGroup(RentACarProjectPermissions.GroupName);
         //Define your own permissions here. Example:
         //myGroup.AddPermission(RentACarProjectPermissions.MyPermission1, L("Permission:MyPermission1"));
+        var cargroup = context.AddGroup(RentACarProjectPermissions.GroupName_Car, L("CarPermission"));
+        var brandgroup = context.AddGroup(RentACarProjectPermissions.GroupName_Brand, L("BrandPermission"));
+        var colorgroup = context.AddGroup(RentACarProjectPermissions.GroupName_Color, L("ColorPermission"));
+        var fuelgroup = context.AddGroup(RentACarProjectPermissions.GroupName_Fuel, L("FuelPermission"));
+        var modelgroup = context.AddGroup(RentACarProjectPermissions.GroupName_Model, L("ModelPermission"));
+        var transmissiongroup = context.AddGroup(RentACarProjectPermissions.GroupName_Transmission, L("TransmissionPermission"));
+
+
+
+        Define_Car_Group(cargroup);
+        Define_Fuel_Group(fuelgroup);
+        Define_Transmission_Group(transmissiongroup);
+        Define_Model_Group(modelgroup);
+        Define_Color_Group(colorgroup);
+        Define_Brand_Group(brandgroup);
+    }
+
+    private void Define_Transmission_Group(PermissionGroupDefinition definition)
+    {
+        var transmissionPermissions = definition.AddPermission(RentACarProjectPermissions.Transmission.Default, L("TransmissionManagment"));
+        transmissionPermissions.AddChild(RentACarProjectPermissions.Transmission.Create, L("Create"));
+        transmissionPermissions.AddChild(RentACarProjectPermissions.Transmission.Update, L("Update"));
+        transmissionPermissions.AddChild(RentACarProjectPermissions.Transmission.Delete, L("Delete"));
+        transmissionPermissions.AddChild(RentACarProjectPermissions.Transmission.Get, L("Get"));
+        transmissionPermissions.AddChild(RentACarProjectPermissions.Transmission.List, L("List"));
+    }
+    private void Define_Model_Group(PermissionGroupDefinition definition)
+    {
+        var modelPermissions = definition.AddPermission(RentACarProjectPermissions.Model.Default, L("CarManagment"));
+        modelPermissions.AddChild(RentACarProjectPermissions.Model.Create, L("Create"));
+        modelPermissions.AddChild(RentACarProjectPermissions.Model.Update, L("Update"));
+        modelPermissions.AddChild(RentACarProjectPermissions.Model.Delete, L("Delete"));
+        modelPermissions.AddChild(RentACarProjectPermissions.Model.Get, L("Get"));
+        modelPermissions.AddChild(RentACarProjectPermissions.Model.List, L("List"));
+    }
+    private void Define_Fuel_Group(PermissionGroupDefinition definition)
+    {
+        var fuelPermission = definition.AddPermission(RentACarProjectPermissions.Fuel.Default, L("FuelManagment"));
+        fuelPermission.AddChild(RentACarProjectPermissions.Fuel.Create, L("Create"));
+        fuelPermission.AddChild(RentACarProjectPermissions.Fuel.Update, L("Update"));
+        fuelPermission.AddChild(RentACarProjectPermissions.Fuel.Delete, L("Delete"));
+        fuelPermission.AddChild(RentACarProjectPermissions.Fuel.Get, L("Get"));
+        fuelPermission.AddChild(RentACarProjectPermissions.Fuel.List, L("List"));
+    }
+    private void Define_Color_Group(PermissionGroupDefinition definition)
+    {
+        var colorPermission = definition.AddPermission(RentACarProjectPermissions.Color.Default, L("ColorManagment"));
+        colorPermission.AddChild(RentACarProjectPermissions.Color.Create, L("Create"));
+        colorPermission.AddChild(RentACarProjectPermissions.Color.Update, L("Update"));
+        colorPermission.AddChild(RentACarProjectPermissions.Color.Delete, L("Delete"));
+        colorPermission.AddChild(RentACarProjectPermissions.Color.Get, L("Get"));
+        colorPermission.AddChild(RentACarProjectPermissions.Color.List, L("List"));
+    }
+    private void Define_Brand_Group(PermissionGroupDefinition definition)
+    {
+        var brandPermissions = definition.AddPermission(RentACarProjectPermissions.Brand.Default, L("BrandManagment"));
+        brandPermissions.AddChild(RentACarProjectPermissions.Brand.Create, L("Create"));
+        brandPermissions.AddChild(RentACarProjectPermissions.Brand.Update, L("Update"));
+        brandPermissions.AddChild(RentACarProjectPermissions.Brand.Delete, L("Delete"));
+        brandPermissions.AddChild(RentACarProjectPermissions.Brand.Get, L("Get"));
+        brandPermissions.AddChild(RentACarProjectPermissions.Brand.List, L("List"));
+    }
+
+    private void Define_Car_Group(PermissionGroupDefinition definition)
+    {
+        var carPermissions = definition.AddPermission(RentACarProjectPermissions.Car.Default, L("CarManagment"));
+        carPermissions.AddChild(RentACarProjectPermissions.Car.Create, L("Create"));
+        carPermissions.AddChild(RentACarProjectPermissions.Car.Update, L("Update"));
+        carPermissions.AddChild(RentACarProjectPermissions.Car.Delete, L("Delete"));
+        carPermissions.AddChild(RentACarProjectPermissions.Car.Get, L("Get"));
+        carPermissions.AddChild(RentACarProjectPermissions.Car.List, L("List"));
     }
 
     private static LocalizableString L(string name)
--- src/RentACarProject.Application.Contracts/Permissions/RentACarProjectPermissions.cs
+++ src/RentACarProject.Application.Contracts/Permissions/RentACarProjectPermissions.cs
@@ -1,9 +1,75 @@
-﻿namespace RentACarProject.Permissions;
+﻿using static RentACarProject.Permissions.RentACarProjectPermissions;
+
+namespace RentACarProject.Permissions;
 
 public static class RentACarProjectPermissions
 {
     public const string GroupName = "RentACarProject";
 
-    //Add your own permission names. Example:
-    //public const string MyPermission1 = GroupName + ".MyPermission1";
+    public const string GroupName_Car = "CarsPermissions";
+    public const string GroupName_Brand = "BrandPermission";
+    public const string GroupName_Color = "ColorPermission";
+    public const string GroupName_Fuel = "FuelPermission";
+    public const string GroupName_Model = "ModelPermission";
+    public const string GroupName_Transmission = "TransmissionPermissions";
+
+    public static class Car
+    {
+        public const string Default = GroupName_Car + ".Car";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string List = Default + ".List";
+    }
+
+    public static class Brand
+    {
+        public const string Default = GroupName_Brand + ".Brand";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string List = Default + ".List";
+    }
+
+    public static class Color
+    {
+        public const string Default = GroupName_Color + ".Color";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string List = Default + ".List";
+    }
+
+    public static class Fuel
+    {
+        public const string Default = GroupName_Fuel + ".Fuel";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string List = Default + ".List";
+    }
+
+    public static class Model
+    {
+        public const string Default = GroupName_Model + ".Model";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string List = Default + ".List";
+    }
+
+    public static class Transmission
+    {
+        public const string Default = GroupName_Transmission + ".Transmission";
+        public const string Create = Default + ".Create";
+        public const string Update = Default + ".Update";
+        public const string Delete = Default + ".Delete";
+        public const string Get = Default + ".Get";
+        public const string List = Default + ".List";
+    }
 }
