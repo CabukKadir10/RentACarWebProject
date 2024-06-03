72a6ebb33f2a9b143f74bde4ab02b9ab107e573c
kadircbk <kadir.c@stratejibilisim.com.tr>
2024-06-03 13:38:28 +0300

    brand de listeleme çalışmıyordu düzelttim

--- src/RentACarProject.Web/Pages/Brand/Brand.js
+++ src/RentACarProject.Web/Pages/Brand/Brand.js
@@ -1,79 +1,91 @@
 var L = abp.localization.getResource('RentACarProject');
 
 $(function () {
-    var _colorAppService = novaHaber.appServices.colors.color;
+    var _brandAppService = rentACarProject.brands.brand;
 
-    var colorAddModal = new abp.ModalManager({
-        viewUrl: '/Color/Add'
+    var brandAddModal = new abp.ModalManager({
+        viewUrl: '/Brand/Add'
     });
-    var colorEditModal = new abp.ModalManager({
-        viewUrl: '/Color/Update'
+    var brandEditModal = new abp.ModalManager({
+        viewUrl: '/Brand/Update'
     });
-
-    colorAddModal.onResult(function (result, response) {
+    brandAddModal.onResult(function (result, response) {
         if (response.statusText == "success") {
             toastr.options.positionClass = 'toast-top-right';
-            abp.notify.success(L('AddSuccess'));
+            abp.notify.success(L('AddSucces'));
         }
+        // if (response.responseText.showDialogProp.resultStatus==0) {
         _dataTable.ajax.reload();
+        //}
     });
-
-    colorEditModal.onResult(function (result, response) {
+    brandEditModal.onResult(function (result, response) {
         if (response.statusText == "success") {
             toastr.options.positionClass = 'toast-top-right';
-            abp.notify.success(L('UpdateSuccess'));
+            abp.notify.success(L('UpdateSucces'));
         }
+        // if (response.responseText.showDialogProp.resultStatus==0) {
         _dataTable.ajax.reload();
+        //}
     });
-
-    $('#ColorAddButton').click(function () {
-        colorAddModal.open();
+    $('#BrandAddButton').click(async function (event) {
+        brandAddModal.open();
     });
-
-    var _dataTable = $('#ColorTable').DataTable(
+    var _dataTable = $('#BrandTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
             serverSide: true,
             paging: true,
             order: [[1, "asc"]],
             searching: true,
-            ajax: abp.libs.datatables.createAjax(_colorAppService.getList),
+            ajax: abp.libs.datatables.createAjax(_brandAppService.getList),
             columnDefs: [
                 {
                     title: L('Actions'),
                     rowAction: {
-                        items: [
-                            {
-                                text: L('Edit'),
-                                visible: function (data) {
-                                    return abp.auth.isGranted('ColorPermission.Color.Update');
+                        items:
+                            [
+                                {
+                                    text: L('Edit'),
+                                    visible: function (data) {
+                                        return abp.auth.isGranted('BrandPermission.Brand.Update');
+                                    },
+                                    action: function (data) {
+                                        brandEditModal.open({
+                                            id: data.record.id,
+                                        });
+                                        ///...
+                                    }
                                 },
-                                action: function (data) {
-                                    colorEditModal.open({ id: data.record.id });
+
+                                {
+                                    text: L('Delete'),
+                                    visible: function (data) {
+                                        return abp.auth.isGranted('BrandPermission.Brand.Delete');
+                                    },
+                                    confirmMessage: function (data) {
+                                        return L(
+                                            'BrandDeletionConfirmationMessage',
+                                            data.record.name
+                                        );
+                                    },
+                                    action: function (data) {
+                                        toastr.options.positionClass = 'toast-top-right';
+                                        _brandAppService
+                                            .delete(data.record.id)
+                                            .then(function () {
+                                                _dataTable.ajax.reload();
+                                                abp.notify.success(L('DeleteSuccess'));
+                                            });
+                                    },
                                 }
-                            },
-                            {
-                                text: L('Delete'),
-                                visible: function (data) {
-                                    return abp.auth.isGranted('ColorPermission.Color.Delete');
-                                },
-                                confirmMessage: function (data) {
-                                    return L('ColorDeletionConfirmationMessage', data.record.Name);
-                                },
-                                action: function (data) {
-                                    toastr.options.positionClass = 'toast-top-right';
-                                    _colorAppService.delete(data.record.id).then(function () {
-                                        _dataTable.ajax.reload();
-                                        abp.notify.success(L('DeleteSuccess'));
-                                    });
-                                }
-                            }
-                        ]
+                            ]
                     }
                 },
+
                 {
                     title: L('Name'),
-                    data: "Name"
+                    data: "name"
                 }
+                
             ]
         })
     );
