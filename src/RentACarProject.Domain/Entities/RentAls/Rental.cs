05ad23cd616b2f3b2a4b4e2f56acf7bb9660d638
kadircbk <kadir.c@stratejibilisim.com.tr>
2024-06-06 13:17:57 +0300

    rental sınıfı olulturuldu

---
+++ src/RentACarProject.Domain/Entities/RentAls/Rental.cs
@@ -0,0 +1,34 @@
+﻿using RentACarProject.Entities.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace RentACarProject.Entities.RentAls
+{
+    public class Rental : Entity<int>
+    {
+        public Rental()
+        {
+
+        }
+        public Rental(int id, int carId, DateTime? rentDate, DateTime? returnDate, int totalPrice) : this()
+        {
+            Id = id;
+            CarId = carId;
+            RentDate = rentDate;
+            ReturnDate = returnDate;
+            TotalPrice = totalPrice;
+        }
+        public int CarId { get; set; }
+        public DateTime? RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int TotalPrice { get; set; }
+
+        public virtual Car Car { get; set; }  // Navigation property
+
+
+    }
+}
